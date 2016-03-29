184c018b3e7b33c0475f2c341bf17dfda7e7a1e6
李昕 <lixin@lixindeMacBook-Pro.local>
2016-03-29 15:08:15 +0800

    Fix Bing image

--- search_api/bing_api/BingSE.py
+++ search_api/bing_api/BingSE.py
@@ -8,36 +8,25 @@ import json
 rc_head = re.compile(r'(?<=<h2>).+?(?=</h2>)')
 rc_url = re.compile(r'(?<=href=").+?(?=")')
 rc_snippet = re.compile(r'(?<=<p>).+?(?=</p>)')
-rc_var = re.compile(r'(?<=var x=_ge\(\').+?(?=\')')
-rc_img = re.compile(r'(?<=x.src=\').+?(?=\')')
-rc_key = re.compile(r'(?<=id=").+?(?=" class="rms_img")')
-rc_src = re.compile(r'(?<=class="rms_img" src=").+?(?=")')
+rc_src = re.compile(r'(?<=src=").+?(?=")')
 
 def BingSE(query):
     para = dict()
     para['q'] = query
-    para['count'] = 50 
+    para['count'] = 50
     url = 'http://www.bing.com/search?' + urllib.urlencode(para)
     try:
         txt = urllib2.urlopen(url).read()
     except Exception as e:
         print e
     else:
-        img = dict()
-        for line in txt.split('\n'):
-            match = rc_var.search(line)
-            if match:
-                img[match.group()] = rc_img.search(line).group()
         soup = BeautifulSoup(txt)
         serp = list()
         for result in soup.find_all('li', class_='b_algo'):
             result = str(result)
-            match = rc_key.search(result)
-            if match:
-                key = match.group()
-                if img.has_key(key):
-                    src = rc_src.search(result).group()
-                    result = result.replace(src, img[key])
+            for img in BeautifulSoup(result).find_all('img', class_='rms_img'):
+                src = rc_src.search(str(img)).group()
+                result = result.replace(src, 'http://www.bing.com' + src)
             d = dict()
             head = rc_head.search(result).group()
             d['url'] = rc_url.search(head).group()
@@ -46,11 +35,6 @@ def BingSE(query):
             d['html'] = result
             serp.append(d)
         res = json.dumps(serp)
-        '''
-        fw = file('Bing.txt', 'a')
-        fw.write(query + '\t' + res + '\n')
-        fw.close()
-        '''
         return res
 
 if __name__ == '__main__':
