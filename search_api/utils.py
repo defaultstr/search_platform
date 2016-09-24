3d5ea2ad88becfc1862569bb29d2037ece203ae7
Mao Jiaxin <maojiaxin@gmail.com>
2016-09-24 16:44:15 +0800

    Filter some document sharing website.
    wenku.baidu.com
    www.docin.com
    www.doc88.com
    www.360doc.com

--- search_api/utils.py
+++ search_api/utils.py
@@ -8,13 +8,29 @@ from .zhihu_api import ZhihuCQA
 from .models import *
 import json
 
+wenku_list = ['http://www.docin.com/', 'http://wenku.baidu.com/', 'http://www.360doc.com/',
+              'http://www.doc88.com/']
+
+
+def validate(result, result_class):
+    if 'url' not in result:
+        return False
+    if result_class is BingResult:
+        url = result['url']
+        for site in wenku_list:
+            if url.startswith(site):
+                return False
+    return True
+
 
 def _crawl(query, search_engine, result_class, results_class):
     results = json.loads(search_engine(query.encode('utf8')))
     res_docs = []
     for r in results:
+        if not validate(r, result_class):
+            continue
         r_doc = result_class(
-            title=r['title'],
+           title=r['title'],
             url=r['url'],
             snippet=r['snippet'],
             html_content=r['html'],
